306ec2ec378dbd3ea180703f0ca5c2fd247f235b
Mike Omohundro <momohundro@carthage.edu>
2017-05-02 16:06:38 -0500

    Vec2D
    Created the Vec2D CPP and HPP files and started populating them.

---
+++ curses-tanks-m/curses-tanks-m/Vect2D.cpp
@@ -0,0 +1,9 @@
+//
+//  Vect2D.cpp
+//  curses-tanks-m
+//
+//  Created by Mike Omohundro on 5/2/17.
+//  Copyright © 2017 Carthage College. All rights reserved.
+//
+
+#include "Vect2D.hpp"
---
+++ curses-tanks-m/curses-tanks-m/Vect2D.hpp
@@ -0,0 +1,19 @@
+//
+//  Vect2D.hpp
+//  curses-tanks-m
+//
+//  Created by Mike Omohundro on 5/2/17.
+//  Copyright © 2017 Carthage College. All rights reserved.
+//
+
+#ifndef Vect2D_hpp
+#define Vect2D_hpp
+
+#include <stdio.h>
+
+#endif /* Vect2D_hpp */
+class Vec2D
+{
+    double x;
+    double y;
+};
